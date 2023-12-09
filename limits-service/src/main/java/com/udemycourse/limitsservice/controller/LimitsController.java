e992c7d91855595cc199e56e8f9a7ddbef8ae573
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-12-09 16:20:20 +0100

    fix: return previous settings

--- limits-service/src/main/java/com/udemycourse/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/udemycourse/limitsservice/controller/LimitsController.java
@@ -1,6 +1,8 @@
 package com.udemycourse.limitsservice.controller;
 
 import com.udemycourse.limitsservice.bean.Limits;
+import com.udemycourse.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,13 @@ import java.util.List;
 @RestController
 public class LimitsController {
 
+
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
