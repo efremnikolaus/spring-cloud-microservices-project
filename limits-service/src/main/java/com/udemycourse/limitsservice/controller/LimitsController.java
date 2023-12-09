d1483acc4c30b1730cbdb9f7c70a4b8e9c81ada1
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-12-09 15:23:03 +0100

    Initial commit

---
+++ limits-service/src/main/java/com/udemycourse/limitsservice/controller/LimitsController.java
@@ -0,0 +1,17 @@
+package com.udemycourse.limitsservice.controller;
+
+import com.udemycourse.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(1, 1000);
+    }
+
+}
